cfa64270d9598b5596205e905d69e9c21456909e
gil-unx <57790408+gil-unx@users.noreply.github.com>
2021-11-04 11:42:15 -0700

    up

--- epsxe/FINDPOS.py
+++ epsxe/FINDPOS.py
@@ -1,15 +1,12 @@
 import struct
-import zlib,io
-def decompress(data):
-	return zlib.decompress(data[0xa:],-15)
+import gzip,io
 def findPos():
-	dat = io.BytesIO(decompress(open("sstates/SLUS_011.15.000","rb").read()))
+	dec = gzip.decompress(open("sstates/SLUS_011.15.004","rb").read())
+	dat = io.BytesIO(dec)
 	dat.seek(0x71cba)
-	
 	xpos = struct.unpack("<i",dat.read(4))[0]
 	zpos = struct.unpack("<i",dat.read(4))[0]
 	ypos = struct.unpack("<i",dat.read(4))[0]
 	print("(Z{0}),(X{1}),(Y{2}),".format(zpos,xpos,ypos))
-	
 	
 findPos()
