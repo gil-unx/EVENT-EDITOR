43fbb5675e3f4368eea10f156f994983666264e5
gil-unx <57790408+gil-unx@users.noreply.github.com>
2021-11-11 15:06:38 +0800

    Update V1.py
    tidak edit lagi dari states. pakai cheat Disable Daily NPC

--- epsxe/V1.py
+++ epsxe/V1.py
@@ -1,9 +1,3 @@
 from EVENT_EXCEL import *
 
 Main(20, -64, 256, -64, 511)
-dec = gzip.decompress(open("sstates/SLUS_011.15.001", "rb").read())
-dat = Br(io.BytesIO(dec))
-dat.seek(0xBD9F2)
-dat.write(b"\x00\x00\x00\x00")
-decNew = gzip.compress(dat.getdata(), compresslevel=9, mtime=None)
-out = open("sstates/SLUS_011.15.001", "wb").write(decNew)
