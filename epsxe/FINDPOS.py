6c19741e9e769bfc56ed57380bbf3980e46b38b5
gil-unx <57790408+gil-unx@users.noreply.github.com>
2023-12-01 19:21:41 +0700

    Update FINDPOS.py
    Cari disemua savestate

--- epsxe/FINDPOS.py
+++ epsxe/FINDPOS.py
@@ -1,14 +1,19 @@
 import struct
-import gzip,io
-def findPos():
-    dec = gzip.decompress(open("sstates/SLUS_011.15.000","rb").read())
+import gzip,io,os
+def printPos(index ):
+    dec = gzip.decompress(open("sstates/SLUS_011.15.{0:03d}".format(index),"rb").read())
     dat = io.BytesIO(dec)
     dat.seek(0x71cba)
     xpos = struct.unpack("<i",dat.read(4))[0]
     zpos = struct.unpack("<i",dat.read(4))[0]
     ypos = struct.unpack("<i",dat.read(4))[0]
-    print("(Z{0}) (X{1}) (Y{2})".format(zpos,xpos,ypos))
-    #dat.seek(0)
-    #out = open("sstates/SLUS_011.15.000.dec","wb").write(dat.read())
+    print("Save State {3}>>(Z{0}) (X{1}) (Y{2})".format(zpos,xpos,ypos,index+1))
+    
+def findPos():
+    for i in range(5):
+        try:
+            printPos(i)
+        except FileNotFoundError:
+            continue
         
 findPos()
