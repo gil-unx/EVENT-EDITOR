2f99b046d0ef39a9ff89749f76748890f4eae600
gil-unx <57790408+gil-unx@users.noreply.github.com>
2024-01-05 13:11:47 +0700

    Perbesar saja,

--- epsxe/PyTexturePacker/main.py
+++ epsxe/PyTexturePacker/main.py
@@ -14,7 +14,7 @@ from PyTexturePacker import Packer
 
 def pack_test(list):
     # create a MaxRectsPacker
-    packer = Packer.create(max_width=1024, max_height=256, enable_rotated = False,bg_color=0xffffff00)
+    packer = Packer.create(max_width=2048, max_height=256, enable_rotated = False,bg_color=0xffffff00)
     # pack texture images under the directory "test_case/" and name the output images as "test_case".
     # "%d" in output file name "test_case%d" is a placeholder,  = Fakswhich is a multipack index, starting with 0.
     packer.pack(list, "PyTexturePacker/list/vram%d", "")
